5ee8c3ec96bddcf9fdf4b08ae0b255d657429db6
Ayush Shrestha <shresthaayush49@gmail.com>
2024-05-14 14:25:02 +0545

    add : cors

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const userRouter = require("./user.routes");
+const trackRouter = require("./track.routes");
 
 module.exports = (app) => {
+  app.use(cors());
   app.use(express.json());
   app.use("/api/v1/user", userRouter);
+  app.use("/api/v1/track", trackRouter);
 };
